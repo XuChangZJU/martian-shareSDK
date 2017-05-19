2b47b29373b38607da93835371748bfd81ba17a0
边边大神 <bla112233@163.com>
2017-05-19 16:18:05 +0800

    sharesdk

---
+++ ios/ShareSDKManager.h
@@ -0,0 +1,14 @@
+//
+//  ShareSDKManager.h
+//  RNShareSDK
+//
+//  Created by Kengsir on 16/6/28.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
+
+@interface ShareSDKManager : RCTEventEmitter <RCTBridgeModule>
+@end
