49c4e1ea25c56df0108336852b3fd10f8d216281
边边大神 <bla112233@163.com>
2017-05-26 18:07:47 +0800

    no message

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bianbian on 2017/5/26.
+ */
+'use strict';
+
+import {
+    NativeModules,
+    NativeAppEventEmitter,
+    Platform
+} from "react-native";
+const ShareSDK = NativeModules.MobLogin;
+
+module.exports = ShareSDK;
